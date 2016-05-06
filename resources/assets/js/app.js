7234ba2aa2c0c00ab49061417fc65d685175964c
John Pope <john@pope.tech>
2016-05-05 20:16:18 -0600

    notes

---
+++ resources/assets/js/app.js
@@ -0,0 +1,5 @@
+// remove fields or add datepicker (mm/dd/yyyy) - jquery UI
+// monthly tracking hide categories link
+// delete button on right side of category field (removes row)
+// reset button on monthly tracker, with are you sure dialog
+// stage for george
