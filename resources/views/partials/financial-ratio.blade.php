af3c142961278317b06af42c01f4c05c0f7a72a7
idbygeorge <george.joeckel@usu.edu>
2016-10-31 15:40:57 -0600

    fix assets to debt ratio, CSS tweaks

--- resources/views/partials/financial-ratio.blade.php
+++ resources/views/partials/financial-ratio.blade.php
@@ -5,32 +5,6 @@
 
   <div class="row">
     <div class="form-group col-xs-12">
-      <label for="liability">{{$ratioType->liability_label}}</label>
-      <input name="liability" id="liability" value="{{$ratio->liability}}" class="form-control">
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="form-group col-xs-12">
-      @if($ratioType->liability_link)
-        <a class="pull-right" href="{{$ratioType->liability_link}}">{{$ratioType->liability_link_text}}</a>
-      @endif
-
-      <div class="help-controls">
-        <a href="#show-help">show help</a>
-        <a href="#hide-help" class="hide">hide help</a>
-      </div>
-
-    </div>
-    <div class="description">
-      <div class="description-content">
-        {!! $ratioType->liability_description !!}
-      </div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="form-group col-xs-12">
       <label for="asset">{{$ratioType->asset_label}}</label>
       <input name="asset" id="asset" value="{{$ratio->asset}}" class="form-control">
     </div>
@@ -51,6 +25,32 @@
     <div class="description">
       <div class="description-content">
         {!! $ratioType->asset_description !!}
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="form-group col-xs-12">
+      <label for="liability">{{$ratioType->liability_label}}</label>
+      <input name="liability" id="liability" value="{{$ratio->liability}}" class="form-control">
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="form-group col-xs-12">
+      @if($ratioType->liability_link)
+        <a class="pull-right" href="{{$ratioType->liability_link}}">{{$ratioType->liability_link_text}}</a>
+      @endif
+
+      <div class="help-controls">
+        <a href="#show-help">show help</a>
+        <a href="#hide-help" class="hide">hide help</a>
+      </div>
+
+    </div>
+    <div class="description">
+      <div class="description-content">
+        {!! $ratioType->liability_description !!}
       </div>
     </div>
   </div>
