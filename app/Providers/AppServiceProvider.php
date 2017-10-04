4e1f40ba934fbb5596e3892b60e14b137bfb7b54
John Pope <john@pope.tech>
2017-10-04 09:29:36 +0000

    update rollbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if ($this->app->environment('production')) {
-            $this->app->register(\Jenssegers\Rollbar\RollbarServiceProvider::class);
-        } elseif($this->app->environment('production')) {
+            $this->app->register(\Rollbar\Laravel\RollbarServiceProvider::class);
+        } elseif($this->app->environment('local')) {
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
         }
     }
