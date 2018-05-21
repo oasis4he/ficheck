a0119952ac1a9c00db795b1be19055fa49b32b10
keith-ferney <keith@ferney.org>
2018-05-21 12:30:29 -0600

    fixed install error

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,11 +28,15 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies($gate);
 
-        // assign new users to the latest group (for now)
-        $defaultGroup = Semester::orderBy('id', 'desc')->first();
+        $pdo = new \PDO(getenv('DB_CONNECTION').':host='.getenv('DB_HOST').';dbname='.getenv('DB_DATABASE'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
+        $query = $pdo->query("SHOW TABLES 'Table_Name' ");
+        if($query){
+          // assign new users to the latest group (for now)
+          $defaultGroup = Semester::orderBy('id', 'desc')->first();
 
-        User::created(function ($user) use ($defaultGroup) {
-            $user->semesters()->attach($defaultGroup->id);
-        });
+          User::created(function ($user) use ($defaultGroup) {
+              $user->semesters()->attach($defaultGroup->id);
+          });
+        }
     }
 }
