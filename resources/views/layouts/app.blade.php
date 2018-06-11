f79ab0d39a021c759f99082abd787af45e63a425
keith-ferney <keith@ferney.org>
2018-06-11 12:38:02 -0600

    update link to js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="/js/jquery-ui.min.js"></script>
-    <script src="/js/app.js?v=1"></script>
+    <script src="{{ URL::to('js/jquery-ui.min.js') }}"></script>
+    <script src="{{ URL::to('js/app.js') }}"></script>
 </body>
 </html>
