82705e45fdd7e7da6e45d1a5b9d835b9e83ded9e
keith-ferney <keith@ferney.org>
2018-06-11 12:22:48 -0600

    fixed js src tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,6 +86,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="/js/jquery-ui.min.js"></script>
-    <script src="/js/app.js"></script>
+    <script src="/js/app.js?v=1"></script>
 </body>
 </html>
